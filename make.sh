f083ae514aec7b94e5df0478eda7cfc531de3584
Leonardo Estacio <mdnssknght@gmail.com>
2022-03-15 22:59:48 -0500

    Implement zip for products in maker script

--- make.sh
+++ make.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-7z a Module.zip ./module/*
+set -e
+
+PRODUCT=$1
+
+if [ -z $PRODUCT ]; then
+    echo "usage: $0 <product>"
+    exit -1
+fi
+
+7z a Module-$PRODUCT-$(date '+%Y%m%d-%H%M%S').zip ./module/* ./product/$PRODUCT
 
