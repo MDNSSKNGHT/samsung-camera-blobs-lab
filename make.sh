582edcdf1ce36733f05e2d0ab6e4b70a9414dd66
Leonardo Estacio <mdnssknght@gmail.com>
2022-03-18 12:01:58 -0500

    Dynamically change module's identifiers when making zip with script

--- make.sh
+++ make.sh
@@ -4,10 +4,39 @@ set -e
 
 PRODUCT=$1
 
+printerr() { echo $1; exit -1; }
+substitute() { sed -i "s/$1/$2/g" $3; }
+
 if [ -z $PRODUCT ]; then
-    echo "usage: $0 <product>"
-    exit -1
+    printerr "usage: $0 <product>"
+else
+    case $PRODUCT in
+    'A50')
+        VERSION=v1.0
+        VERSIONCODE=1
+        ;;
+    'A51')
+        VERSION=v1.0
+        VERSIONCODE=1
+        ;;
+    'N10+')
+        VERSION=v1.0
+        VERSIONCODE=1
+        ;;
+    *)
+        printerr "Couldn't find folder for $PRODUCT"
+    esac
 fi
+
+# Replace identifiers in module/module.prop
+substitute 'TARGET' $PRODUCT module/module.prop
+substitute 'VERSIONCODE' $VERSIONCODE module/module.prop
+substitute 'VERSION' $VERSION module/module.prop
+
+# Replace identifiers in module/update.json
+substitute 'TARGET' $PRODUCT module/update.json
+substitute '-1' $VERSIONCODE module/update.json
+substitute 'VERSION' $VERSION module/update.json
 
 7z a Module-$PRODUCT-$(date '+%Y%m%d-%H%M%S').zip ./module/* ./product/$PRODUCT/*
 
